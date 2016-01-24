ad38d924aac889b83a46a77e37429ec05e604d80
Henry Van Styn <vanstyn@intellitree.com>
2016-01-24 08:31:26 -0500

    Generalize scrape logic into Rapi::EBO::Scrape object class

--- devel/scrape-populate.pl
+++ devel/scrape-populate.pl
@@ -5,15 +5,14 @@ use strict;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 
+use Rapi::EBO::Scrape;
+
 use Rapi::EBO::Model::DB;
 my $db = Rapi::EBO::Model::DB->_one_off_connect;
 my $datasetRs   = $db->resultset('Dataset');
 my $candidateRs = $db->resultset('Candidate');
 
 use Path::Class qw(file dir);
-use Web::Scraper;
-use DateTime::Format::Flexible;
-
 use RapidApp::Util ':all';
 
 my $Dir = dir( $ARGV[0] )->resolve;
@@ -23,20 +22,10 @@ for my $File ( $Dir->children ) {
   print "\n[$File]\n";
 
   my $content = $File->slurp;
-
-  my $scraper = scraper {
-    process 'body > p:first-child > i'    => 'lastUpd'  => 'TEXT';
-    process 'tr > td:nth-child(1) > img'  => 'dImg[]' => '@src';
-    process 'tr > td:nth-child(2) > p'    => 'dPct[]' => 'TEXT';
-    process 'tr > td:nth-child(3) > img'  => 'rImg[]' => '@src';
-    process 'tr > td:nth-child(4) > p'    => 'rPct[]' => 'TEXT';
-    process 'tr > td:nth-child(5) > img'  => 'gImg[]' => '@src';
-    process 'tr > td:nth-child(6) > p'    => 'gPct[]' => 'TEXT';
-  };
-
-  my $res = $scraper->scrape( $content );
-
-  my $timestamp = &parse_timestamp($res->{lastUpd});
+  
+  my $Scrape = Rapi::EBO::Scrape->new({ html => $content });
+  
+  my $timestamp = $Scrape->timestamp;
   
   print " --> dataset timestamp $timestamp ";
   
@@ -44,49 +33,8 @@ for my $File ( $Dir->children ) {
     print "[exists, skipping]";
     next;
   }
-
-
-  my $Data = {
-    _timestamp => $timestamp,
-    Democratic => {},
-    Republican => {},
-    Presidency => {}
-  };
-
-
-  for my $img (@{$res->{dImg}}) {
-    my @parts = split(/\//,$img);
-    my $fn = pop @parts;
-    $fn =~ /^(\w+)\..+$/;
-    my $name = $1;
-    
-    my $pct = (shift @{$res->{dPct}});
-    $pct =~ s/\%//;
-    $Data->{Democratic}{$name} = { img => $img, pct => $pct };
-  }
-
-  for my $img (@{$res->{rImg}}) {
-    my @parts = split(/\//,$img);
-    my $fn = pop @parts;
-    $fn =~ /^(\w+)\..+$/;
-    my $name = $1;
-    
-    my $pct = (shift @{$res->{rPct}});
-    $pct =~ s/\%//;
-    $Data->{Republican}{$name} = { img => $img, pct => $pct };
-  }
-
-  for my $img (@{$res->{gImg}}) {
-    my @parts = split(/\//,$img);
-    my $fn = pop @parts;
-    $fn =~ /^(\w+)\..+$/;
-    my $name = $1;
-    
-    my $pct = (shift @{$res->{gPct}});
-    $pct =~ s/\%//;
-    $Data->{Presidency}{$name} = { img => $img, pct => $pct };
-  }
   
+  my $Data = $Scrape->Data;
   
   my @ticks = ();
   
@@ -119,38 +67,7 @@ for my $File ( $Dir->children ) {
   
   print "[populated]";
   
-  
-  #scream(\@ticks);
-  
-  
-  #scream( $Data );
-  
-
 
 }
 
 
-
-
-
-
-
-sub parse_timestamp {
-  my $upd = shift;
-
-  my $ts = (split(/Last updated\:\s+/, $upd ,2))[1];
-
-  my ($time,$date) = split(/ on /,$ts,2);
-  my $tz;
-  ($time,$tz) = split(/\s+/,$time,2);
-  my $pm = $time =~ /PM/i ? 1 : 0;
-  $time =~ s/.{2}$//;
-  my ($h,$m) = split(/\:/,$time,2);
-  $h = $h + 12 if ($pm && $h < 12);
-
-  my $dt = DateTime::Format::Flexible->parse_datetime($date);
-  $dt->set_hour($h);
-  $dt->set_minute($m);
-  
-  return $dt->ymd('-') . ' ' . $dt->hms(':');
-}
---
+++ lib/Rapi/EBO/Scrape.pm
@@ -0,0 +1,117 @@
+package Rapi::EBO::Scrape;
+
+use strict;
+use warnings;
+
+# ABSTRACT: scraper for electionbettingodds.com homepage 
+
+use Moo;
+use Types::Standard qw(:all);
+
+use Web::Scraper;
+use DateTime::Format::Flexible;
+
+has 'html', is => 'ro', isa => Str, required => 1;
+
+has 'web_scraper', is => 'ro', default => sub {
+  scraper {
+    process 'body > p:first-child > i'    => 'lastUpd'  => 'TEXT';
+    process 'tr > td:nth-child(1) > img'  => 'dImg[]' => '@src';
+    process 'tr > td:nth-child(2) > p'    => 'dPct[]' => 'TEXT';
+    process 'tr > td:nth-child(3) > img'  => 'rImg[]' => '@src';
+    process 'tr > td:nth-child(4) > p'    => 'rPct[]' => 'TEXT';
+    process 'tr > td:nth-child(5) > img'  => 'gImg[]' => '@src';
+    process 'tr > td:nth-child(6) > p'    => 'gPct[]' => 'TEXT';
+  };
+}, isa => InstanceOf['Web::Scraper'], init_arg => undef;
+
+
+has 'scrape_result', is => 'ro', lazy => 1, default => sub {
+  my $self = shift;
+  $self->web_scraper->scrape( $self->html );
+}, isa => HashRef, init_arg => undef;
+
+
+has 'timestamp_dt', is => 'ro', lazy => 1, default => sub {
+  my $self = shift;
+  $self->_parse_timestamp_dt( $self->scrape_result->{lastUpd} )
+}, isa => InstanceOf['DateTime'], init_arg => undef;
+
+has 'timestamp', is => 'ro', lazy => 1, default => sub {
+  my $self = shift;
+  my $dt = $self->timestamp_dt;
+  return $dt->ymd('-') . ' ' . $dt->hms(':');
+}, isa => Str, init_arg => undef;
+
+has 'Data', is => 'ro', lazy => 1, default => sub {
+  my $self = shift;
+  
+  my $res = $self->scrape_result;
+  
+  my $Data = {
+    Democratic => {},
+    Republican => {},
+    Presidency => {}
+  };
+
+  for my $img (@{$res->{dImg}}) {
+    my @parts = split(/\//,$img);
+    my $fn = pop @parts;
+    $fn =~ /^(\w+)\..+$/;
+    my $name = $1;
+    
+    my $pct = (shift @{$res->{dPct}});
+    $pct =~ s/\%//;
+    $Data->{Democratic}{$name} = { img => $img, pct => $pct };
+  }
+
+  for my $img (@{$res->{rImg}}) {
+    my @parts = split(/\//,$img);
+    my $fn = pop @parts;
+    $fn =~ /^(\w+)\..+$/;
+    my $name = $1;
+    
+    my $pct = (shift @{$res->{rPct}});
+    $pct =~ s/\%//;
+    $Data->{Republican}{$name} = { img => $img, pct => $pct };
+  }
+
+  for my $img (@{$res->{gImg}}) {
+    my @parts = split(/\//,$img);
+    my $fn = pop @parts;
+    $fn =~ /^(\w+)\..+$/;
+    my $name = $1;
+    
+    my $pct = (shift @{$res->{gPct}});
+    $pct =~ s/\%//;
+    $Data->{Presidency}{$name} = { img => $img, pct => $pct };
+  }
+  
+  return $Data
+
+}, isa => HashRef, init_arg => undef;
+
+
+
+sub _parse_timestamp_dt {
+  my $self = shift;
+  my $upd = shift;
+
+  my $ts = (split(/Last updated\:\s+/, $upd ,2))[1];
+
+  my ($time,$date) = split(/ on /,$ts,2);
+  my $tz;
+  ($time,$tz) = split(/\s+/,$time,2);
+  my $pm = $time =~ /PM/i ? 1 : 0;
+  $time =~ s/.{2}$//;
+  my ($h,$m) = split(/\:/,$time,2);
+  $h = $h + 12 if ($pm && $h < 12);
+
+  my $dt = DateTime::Format::Flexible->parse_datetime($date);
+  $dt->set_hour($h);
+  $dt->set_minute($m);
+  
+  return $dt
+}
+
+1;
