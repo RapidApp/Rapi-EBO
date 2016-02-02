589167946a47d463d2203e5f95dbb58c3658aa11
Henry Van Styn <vanstyn@intellitree.com>
2016-02-02 13:19:05 -0500

    slurp_photos - added --force and experimental --shrink

--- devel/slurp_photos.pl
+++ devel/slurp_photos.pl
@@ -6,11 +6,16 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 
 use LWP::Simple;
+use IPC::Cmd qw[can_run run_forked];
+use Path::Class qw/file dir/;
 
 use Rapi::EBO;
 my $db = Rapi::EBO->model('DB')->schema;
 my $CAS = Rapi::EBO->controller('SimpleCAS');
 
+my %flags = map {$_=>1} @ARGV;
+
+die 'morgify command not found, cannot shrink' if ($flags{'--shrink'} && !can_run('morgify'));
 
 print "\n\nScanning for photo changes...\n\n";
 
@@ -26,10 +31,33 @@ for my $Candidate ($db->resultset('Candidate')->all) {
   
   print "[success, " . (length $content) . ' bytes] ';
   
+  if($flags{'--shrink'}) {
+    my $tmp = file('_tmp-img-' . time . '-' . $fn);
+    die "$tmp already exists? giving up" if (-e $tmp);
+    $tmp->spew( $content );
+    
+    my @cmd = ('morgify','-resize','60x60',$tmp);
+    
+    print "\n   " . join(' ',@cmd);
+    
+    my $result = run_forked(\@cmd);
+    my $exit = $result->{exit_code};
+    
+    print " [exit: $exit]\n";
+    die "\n" . $result->{err_msg} if ($exit);
+    
+    $content = $tmp->slurp;
+    unlink $tmp;
+  
+  }
+  
+  
+  
+  
   my $checksum = $CAS->Store->add_content( $content ) or die " err adding";
   
   my $val = join('/',$checksum,$fn);
-  if($Candidate->photo_cas && $Candidate->photo_cas eq $val) {
+  if(! $flags{'--force'} && $Candidate->photo_cas && $Candidate->photo_cas eq $val) {
     print "- no changes\n";
     next;
   }
